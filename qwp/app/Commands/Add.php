8e7020a0fbfece1b05f9aecc52fdb99fb91d7345
Ross Wintle <rosswintle@users.noreply.github.com>
2023-07-24 22:32:12 +0100

    All working now, but needs refactoring

--- qwp/app/Commands/Add.php
+++ qwp/app/Commands/Add.php
@@ -4,7 +4,11 @@ namespace App\Commands;
 
 use App\Services\WpCli;
 use App\Services\SiteIndex;
+use Illuminate\Support\Str;
 use App\Services\WpCoreVersion;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use LaravelZero\Framework\Commands\Command;
 
 class Add extends Command
@@ -35,36 +39,30 @@ class Add extends Command
     protected string $installPath;
 
     /**
-     * Execute the console command.
-     *
-     * @return mixed
+     * Get the version option - interprets 'latest' as the latest version.
      */
-    public function handle(SiteIndex $index)
+    public function getVersionOption() : string
     {
         if ($this->option('wp-version') === 'latest') {
-            $this->version = app(WpCoreVersion::class)->getLatestVersion();
+            return app(WpCoreVersion::class)->getLatestVersion();
         } else {
-            $this->version = $this->option('wp-version');
+            return $this->option('wp-version');
         }
+    }
 
-        $this->installPath = $this->option('path') ?? ( getcwd() . '/' . $this->argument('name') );
+    /**
+     * Get the install path option - defaults to a subdirectory of the current directory.
+     */
+    public function getInstallPathOption() : string
+    {
+        return $this->option('path') ?? ( getcwd() . '/' . $this->argument('name') );
+    }
 
-        // CHECK NAME AND PATH DON'T ALREADY EXIST
-        // Check for an existing directory
-        if (file_exists($this->installPath)) {
-            $this->error("Directory already exists: " . $this->installPath);
-            return;
-        }
-
-        $this->info("Adding site: " . $this->argument('name'));
-
-        // Make the directory
-        mkdir($this->installPath);
-
-        $coreFilesPath = app(WpCoreVersion::class)->getPath($this->option('wp-version'));
-
-
-        // Link the core files to the sites directory
+    /**
+     * Link the core files to the sites directory.
+     */
+    public function linkCoreFiles(): void
+    {
         $filesToLink = [
             '/wp-admin',
             '/wp-includes',
@@ -73,16 +71,60 @@ class Add extends Command
             '/xmlrpc.php',
         ];
 
+        $coreFilesPath = app(WpCoreVersion::class)->getPath($this->version);
+
         $pathsToLink = array_map(
             fn ($file) => $coreFilesPath . $file,
             $filesToLink
         );
 
+        // TODO: Error handling
         exec('ln -s ' . implode(' ', $pathsToLink) . ' ' . $this->installPath, $output, $resultCode);
+    }
 
-        // copy config (have to copy this from source, not the symlink)
-        // or wp config create
+    /**
+     * Create the wp-config.php file.
+     */
+    public function createWpConfigFile(): void
+    {
+        // TODO: Error handling
         app(WpCli::class)->run('config create --dbname=localhost --dbuser=unused --skip-check --insecure --path=' . $this->installPath);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(SiteIndex $index)
+    {
+        $this->version = $this->getVersionOption();
+
+        $this->installPath = $this->getInstallPathOption();
+
+        // CHECK NAME AND PATH DON'T ALREADY EXIST
+        // Check for an existing directory
+        if (file_exists($this->installPath)) {
+            $this->error("Directory already exists: " . $this->installPath);
+            return;
+        }
+
+        // Check for an existing site in the index
+        if ($index->exists($this->argument('name'))) {
+            $this->error("Site already exists: " . $this->argument('name'));
+            return;
+        }
+
+        $this->info("Adding site: " . $this->argument('name'));
+
+        // Make the directory
+        mkdir($this->installPath);
+
+        // Link the core files to the sites directory
+        $this->linkCoreFiles();
+
+        // or wp config create
+        $this->createWpConfigFile();
 
         // make wp-content directory
         mkdir($this->installPath . '/wp-content');
@@ -92,26 +134,66 @@ class Add extends Command
         mkdir($this->installPath . '/wp-content/themes');
 
         // Check SQLite plugin exists and get it if required
+        if (!Storage::directoryExists(Storage::path('plugins'))) {
+            Storage::createDirectory(Storage::path('plugins'));
+        }
 
-        // copy sqlite plugin
-        // cp -r $QWP_DIR/sqlite-database-integration $INSTALL_DIR/wp-content/plugins
+        if (Storage::exists(Storage::path('plugins/sqlite-database-integration.zip'))) {
+            $this->info("Using existing SQLite plugin");
+        } else {
+            $this->info("Fetching SQLite plugin");
+            Http::withOptions(['sink' => Storage::path('plugins/sqlite-database-integration.zip')])
+                ->get('https://downloads.wordpress.org/plugin/sqlite-database-integration.zip');
+            // Unzip the plugin
+            $this->info("Unzipping SQLite plugin");
+            $zip = new \ZipArchive;
+            $zip->open(Storage::path('plugins/sqlite-database-integration.zip'));
+            $zip->extractTo(Storage::path('plugins'));
+        }
 
-        // copy plugins/sqlite-database-integration/db.copy to wp-content/db.php'
-        // cp -r $QWP_DIR/sqlite-database-integration/db.copy $INSTALL_DIR/wp-content/db.php
+        File::copyDirectory(Storage::path('plugins/sqlite-database-integration'), $this->installPath . '/wp-content/plugins/sqlite-database-integration');
 
-        // copy in a theme?
-        // cp -r $QWP_DIR/$VERSION_REQUESTED/wp-content/themes/* $INSTALL_DIR/wp-content/themes/
+        File::copy(Storage::path('plugins/sqlite-database-integration/db.copy'), $this->installPath . '/wp-content/db.php');
 
         // from https://github.com/WordPress/sqlite-database-integration/issues/7#issuecomment-1563465590
-        // sed -i '' 's#{SQLITE_IMPLEMENTATION_FOLDER_PATH}#${INSTALL_DIR}/wp-content/plugins/sqlite-database-integration#' ${INSTALL_DIR}/wp-content/db.php
-        // sed -i '' 's#{SQLITE_PLUGIN}#sqlite-database-integration/load.php#' ${INSTALL_DIR}/wp-content/db.php
-        // mkdir $INSTALL_DIR/wp-content/database && touch $INSTALL_DIR/wp-content/database/.ht.sqlite
+        $dbPhp = file_get_contents($this->installPath . '/wp-content/db.php');
+        // Replace the placeholders with the correct values
+        $dbPhp = str_replace(
+            [
+                '{SQLITE_IMPLEMENTATION_FOLDER_PATH}',
+                '{SQLITE_PLUGIN}',
+            ],
+            [
+                $this->installPath . '/wp-content/plugins/sqlite-database-integration',
+                'sqlite-database-integration/load.php',
+            ],
+            $dbPhp
+        );
+        File::makeDirectory($this->installPath . '/wp-content/database');
+        File::put(
+            $this->installPath . '/wp-content/database/.ht.sqlite',
+            ''
+        );
+
+        // copy in a theme?
+        // TODO: Make this a method of the CoreVersion service
+        $themeDirs = Storage::directories('wordpress/' . $this->version . '/wp-content/themes');
+        foreach ($themeDirs as $dir) {
+            echo "Copying theme: $dir\n";
+            File::copyDirectory(
+                Storage::path($dir),
+                $this->installPath . '/wp-content/themes/' . Str::afterLast($dir, '/')
+            );
+        }
 
         // Could add --locale
-        // $WP_CLI core install --url=http://localhost:8001 --title="A New Hope" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --skip-email
+        app(WpCli::class)->run('core install --url=http://localhost:8001 --title="A New Hope" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --skip-email --path=' . $this->installPath);
 
         // copy the router.php in
-        // cp $SCRIPT_DIR/router.php $INSTALL_DIR
+        File::copy(
+            app_path('router-template.php'),
+            $this->installPath . '/router.php'
+        );
 
         // php -S localhost:<port> <path>/router.php
         // php -S localhost:8001 $INSTALL_DIR/router.php
