ef099bf0546cc0e0ecd1ec1c985d22940c31e942
Ross Wintle <rosswintle@users.noreply.github.com>
2026-06-28 20:19:01 +0100

    Add more media types to the router

--- app/router-template.php
+++ app/router-template.php
@@ -1,7 +1,7 @@
 <?php
 // Might be good to copy from the bottom of https://github.com/wp-cli/server-command/blob/main/router.php
 define('ABSPATH', __DIR__ . '/');
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js|webp|aiff)$/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js|webp|aiff|mp4|mp3|mov|json|wav)$/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;    // serve the requested resource as-is.
 } elseif (preg_match('/\.php$/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     require ABSPATH . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
